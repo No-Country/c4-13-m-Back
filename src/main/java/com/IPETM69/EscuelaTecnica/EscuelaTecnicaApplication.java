7e468eb81e5947ae96ccd87ff220278ab32d7ea8
Yane05 <yane.05@hotmail.com>
2022-03-25 12:00:53 -0300

    commit initial

---
+++ src/main/java/com/IPETM69/EscuelaTecnica/EscuelaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.IPETM69.EscuelaTecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscuelaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscuelaTecnicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IPETM69/EscuelaTecnica/EscuelaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IPETM69.EscuelaTecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscuelaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
