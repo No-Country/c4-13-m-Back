c53663b5fe87f22ba75133843953f37fed64d273
Carolina Puebla <puebla.carolina@gmail.com>
2022-05-09 11:11:52 -0300

    Commit con anotacion del service

--- src/main/java/com/IPETM69/EscuelaTecnica/service/impl/SettingServiceImpl.java
+++ src/main/java/com/IPETM69/EscuelaTecnica/service/impl/SettingServiceImpl.java
@@ -7,7 +7,9 @@ import com.IPETM69.EscuelaTecnica.dto.response.SettingDTOResponse;
 import com.IPETM69.EscuelaTecnica.mapper.SettingMapper;
 import com.IPETM69.EscuelaTecnica.service.SettingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SettingServiceImpl implements SettingService {
 
     @Autowired
